ac8e76cd9a060b4bc64862010d94a1f1a07765df
Durga1534 <durgaprasadpc143@gmail.com>
2026-08-19 10:46:26 +0530

    fix: fixed incorrect env variable in drizzle.config.ts file

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -9,6 +9,6 @@ export default defineConfig({
     out: "./drizzle",
     dialect: "postgresql",
     dbCredentials: {
-        url: process.env.DATABSE_URL!,
+        url: process.env.DATABASE_URL!,
     },
 });
